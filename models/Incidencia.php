848a0a07a0a2d59133906af8084cba79cbe1553f
Pablo Romero <pabloromeroagudo@gmail.com>
2024-11-08 19:35:48 +0100

    feat: :sparkles: Añadir clase incidencia y DAO completo

--- models/Incidencia.php
+++ models/Incidencia.php
@@ -1,4 +1,129 @@
 <?php
 
+declare(strict_types=1);
+
 class Incidencia {
+  private int $idincidencia;
+  private int $idmaquina;
+  private int $idproducto;
+  private int $idubicacion;
+  private string $categoria;
+  private int $stock;
+  private string $severidad;
+  private string $estado;
+  private string $descripcion;
+  private DateTime $fecharegistro;
+  private DateTime|null $fecharesolucion;
+  private string|null $solucion;
+
+  public function __construct(array $data) {
+    $this->setIdIncidencia($data["idincidencia"]);
+    $this->setIdMaquina($data["idmaquina"]);
+    $this->setIdProducto($data["idproducto"]);
+    $this->setIdUbicacion($data["idubicacion"]);
+    $this->setCategoria($data["categoria"]);
+    $this->setStock($data["stock"]);
+    $this->setSeveridad($data["severidad"]);
+    $this->setEstado($data["estado"]);
+    $this->setDescripcion($data["descripcion"]);
+    $this->setFechaRegistro(new DateTime($data["fecharegistro"]));
+    $this->setFechaResolucion(isset($data["fecharesolucion"])  ? new DateTime($data["fecharesolucion"]) : null);
+    $this->setSolucion($data["solucion"]);
+  }
+
+  public function setIdIncidencia(int $idIncidencia) {
+    $this->idincidencia = $idIncidencia;
+  }
+
+  public function getIdIncidencia(): int {
+    return $this->idincidencia;
+  }
+
+  public function setIdMaquina(int $idMaquina) {
+    $this->idmaquina = $idMaquina;
+  }
+
+  public function getIdMaquina(): int {
+    return $this->idmaquina;
+  }
+
+  public function setIdProducto(int $idProducto) {
+    $this->idproducto = $idProducto;
+  }
+
+  public function getIdProducto(): int {
+    return $this->idproducto;
+  }
+
+  public function setIdUbicacion(int $idUbicacion) {
+    $this->idubicacion = $idUbicacion;
+  }
+
+  public function getIdUbicacion(): int {
+    return $this->idubicacion;
+  }
+
+  public function setCategoria(string $categoria) {
+    $this->categoria = $categoria;
+  }
+
+  public function getCategoria(): string {
+    return $this->categoria;
+  }
+
+  public function setStock(int $stock) {
+    $this->stock = $stock;
+  }
+
+  public function getStock(): int {
+    return $this->stock;
+  }
+
+  public function setSeveridad(string $severidad) {
+    $this->severidad = $severidad;
+  }
+
+  public function getSeveridad(): string {
+    return $this->severidad;
+  }
+
+  public function setEstado(string $estado) {
+    $this->estado = $estado;
+  }
+
+  public function getEstado(): string {
+    return $this->estado;
+  }
+
+  public function setDescripcion(string $descripcion) {
+    $this->descripcion = $descripcion;
+  }
+
+  public function getDescripcion(): string {
+    return $this->descripcion;
+  }
+
+  public function setFechaRegistro(DateTime $fechaRegistro) {
+    $this->fecharegistro = $fechaRegistro;
+  }
+
+  public function getFechaRegistro(): DateTime {
+    return $this->fecharegistro;
+  }
+
+  public function setFechaResolucion(DateTime|null $fechaResolucion) {
+    $this->fecharesolucion = $fechaResolucion;
+  }
+
+  public function getFechaResolucion(): DateTime|null {
+    return $this->fecharesolucion;
+  }
+
+  public function setSolucion(string|null $solucion) {
+    $this->solucion = $solucion;
+  }
+
+  public function getSolucion(): string|null {
+    return $this->solucion;
+  }
 }
--- modules/m4/DAO/IncidenciaDao.php
+++ modules/m4/DAO/IncidenciaDao.php
@@ -1,6 +1,157 @@
 <?php
 
 declare(strict_types=1);
+require_once '../../../models/Incidencia.php';
+require_once '../../../models/BD/BD.php';
+require_once '../utils/functions.php';
 
 class IncidenciaDao {
+
+  public function __construct() {
+  }
+
+  public function insert(Incidencia $incidencia): bool {
+    try {
+      $dbh = BD::getInstance();
+      $stmt = $dbh->prepare(
+        "INSERT INTO incidencias 
+        ( idmaquina
+        , idproducto
+        , idubicacion
+        , categoria
+        , stock
+        , severidad
+        , estado
+        , descripcion
+        , fecharegistro
+        ) VALUES (
+          :idmaquina
+        , :idproducto
+        , :idubicacion
+        , :categoria
+        , :stock
+        , :severidad
+        , :estado
+        , :descripcion
+        , :fecharegistro
+        )"
+      );
+
+      $stmt->bindValue(":idmaquina",      $incidencia->getIdIncidencia());
+      $stmt->bindValue(":idproducto",     $incidencia->getIdProducto());
+      $stmt->bindValue(":idubicacion",    $incidencia->getIdUbicacion());
+      $stmt->bindValue(":categoria",      $incidencia->getCategoria());
+      $stmt->bindValue(":stock",          $incidencia->getStock());
+      $stmt->bindValue(":severidad",      $incidencia->getSeveridad());
+      $stmt->bindValue(":estado",         $incidencia->getEstado());
+      $stmt->bindValue(":descripcion",    $incidencia->getDescripcion());
+      $stmt->bindValue(":fecharegistro",  $incidencia->getFechaRegistro());
+
+      $insert = $stmt->execute();
+
+      BD::close();
+      return $insert;
+    } catch (Throwable $e) {
+      print_r(mostrar_error($e));
+      BD::close();
+
+      return false;
+    }
+  }
+
+  public function findAll(): array|null {
+    try {
+      $dbh = BD::getInstance();
+      $stmt = $dbh->prepare("SELECT * FROM incidencias");
+      $stmt->execute();
+
+      $fetch = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $incidencias = [];
+
+      foreach ($fetch as $incidencia) {
+        $incidencias[] = new Incidencia($incidencia);
+      }
+
+      BD::close();
+      return $incidencias;
+    } catch (Throwable $e) {
+      print_r(mostrar_error($e));
+
+      BD::close();
+      return null;
+    }
+  }
+
+  public function findById(int $id): Incidencia|null {
+    try {
+      $dbh = BD::getInstance();
+      $stmt = $dbh->prepare("SELECT * FROM incidencias WHERE idincidencia = :id");
+      $stmt->bindValue(":id", $id);
+      $stmt->execute();
+
+      $incidencia = new Incidencia($stmt->fetch(PDO::FETCH_ASSOC));
+
+      BD::close();
+      return $incidencia;
+    } catch (Throwable $e) {
+      print_r(mostrar_error($e));
+
+      BD::close();
+      return null;
+    }
+  }
+
+  public function update(Incidencia $incidencia): Incidencia|null {
+    try {
+      $dbh = BD::getInstance();
+      $id = $incidencia->getIdIncidencia();
+      $incidenciaPrevia = $this->findById($id);
+      $stmt = $dbh->prepare(
+        "UPDATE incidencias 
+        set
+          estado = :estado,
+          fecharesolucion = :fecharesolucion,
+          solucion = :solucion
+        WHERE 
+          idincidencia = :id"
+      );
+      $stmt->bindValue(":estado", $incidencia->getEstado());
+      $stmt->bindValue(":fecharesolucion", date("Y-m-d H:i:s"));
+      $stmt->bindValue(":solucion", $incidencia->getSolucion());
+      $stmt->bindValue(":id", $id);
+
+      return ["antiguo" => $incidenciaPrevia, "nuevo" => $this->findById($id)];
+      BD::close();
+      return $incidencia;
+    } catch (Throwable $e) {
+      print_r(mostrar_error($e));
+      BD::close();
+
+      return null;
+    }
+  }
+
+  public function delete(int $id): Incidencia|null {
+    try {
+      $dbh = BD::getInstance();
+      $incidencia = $this->findById($id);
+      $stmt = $dbh->prepare("DELETE FROM incidencias WHERE idincidencia = :id");
+      $stmt->bindValue(":id", $id);
+      $stmt->execute();
+
+
+      BD::close();
+      return $incidencia;
+    } catch (Throwable $e) {
+      print_r(mostrar_error($e));
+      BD::close();
+
+      return null;
+    }
+  }
+
+
+  // TODO
+  public function findByFiltros() {
+  }
 }
